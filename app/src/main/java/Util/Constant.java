82e869f89c927786641f9bbed6ff50704e4c6356
楠 <34267492+Zed-a@users.noreply.github.com>
2018-10-17 18:55:06 +0800

    Update Constant.java

--- app/src/main/java/Util/Constant.java
+++ app/src/main/java/Util/Constant.java
@@ -1,28 +1,98 @@
 package Util;
 
-import Entity.Food;
-
-/**
- * Created by fanmiaomiao on 2018/3/27.
- */
-
 public class Constant {
-    static public String[] Foods ={"type_staple","type_soup","type_seafood","type_fruit_vegetable","type_breakfast","type_western","type_dessert"};
-    public enum Nutrtions{
-        VEGETABLES("vegetables",1),EGG("egg",2),OIL("oil",3),MILK("milk",4),MEAT("meats",5),FISH("fish",6),GRAIN("grain",7);
+    static public String[] Foods = {"type_staple", "type_soup", "type_seafood", "type_fruit_vegetable", "type_breakfast", "type_western", "type_dessert"};
+
+    public enum Nutrtions {
+        VEGETABLES("vegetables", 1), EGG("egg", 2), OIL("oil", 3), MILK("milk", 4), MEAT("meats", 5), FISH("fish", 6), GRAIN("grain", 7);
 
         private String name;
         private int ID;
-        Nutrtions(String na,int id){
-            this.name=na;
+
+        Nutrtions(String na, int id) {
+            this.name = na;
             this.ID = id;
         }
+
         @Override
         public String toString() {
-            return "type_"+name;
+            return "type_" + name;
         }
-        public int getID(){
+
+        public int getID() {
             return ID;
         }
     }
+
+    //本地访问路径
+    public static String SERVE_URL="http://172.31.174.104:8090/servlet/user/ParseJSONServlet";
+
+    //服务器访问路径
+//    public static String SERVE_URL="http://115.159.127.223/ieat/servlet/user/ParseJSONServlet";
+
+
+    public static final String CHARSET="utf-8";
+
+    public static final String REQUEST_TYPE="request_type";
+    public static final String REGISTER="register";
+    public static final String LOGIN="login";
+
+    public static final String LOGIN_TYPE="login_type";
+    public static final String REGISTER_TYPE="register_type";
+    public static final String QQ="qq";//QQ登录或注册
+    public static final String WEICHAT="weichat";//微信登录或注册
+    public static final String TEL="tel";//电话号码登录或注册
+
+
+    /**
+     * USER INFO
+     */
+    public static final String USERID = "userId";
+    public static final String NICKNAME="nickName";
+    public static final String HEIGHT="height";
+    public static final String WEIGHT="weight";
+    public static final String AGE="age";
+    public static final String SEX="sex";
+    public static final String ACCOUNT="account";
+    public static final String PASSWORD="password";
+
+
+    /**
+     * FOOD INFO
+     */
+    public static final String FOODID="foodId";
+    public static final String FOODNAME="foodName";
+    public static final String MATERIAL="material";
+    public static final String STEP="step";
+    public static final String IMAGE="imageURL";
+    public static final String FOODSTAR="star";
+
+
+    public static final String NUM_PEOPLE="num_people";
+    public static final String NUM_FOOD="num_food";
+    public static final String AVOIDFOODTYPE="avoidFoodType";
+    public static final String SUITPEOPLE="suit_people";
+    public static final String USERFLAVOUR="userFlavour";
+
+
+    /**
+     * ACTION
+     */
+    public static final String GETUSERINFO="getUserInfo";
+    public static final String GETFOODINFOBYID="getFoodInfoById";
+    public static final String GETPUBLISHFOOD="getPublishFood";
+    public static final String GETFOODBYMATERIAL="getFoodByMaterial";
+    public static final String FOODCLICK="food_click";
+
+
+    public static final String RESPONSE="response";
+    public static final String NOTICE="notice";
+    public static final String ISCOLLECTION="isCollection";
+
+
+
+    public static final String STATUSCODE="status_code";
+    public static final int FAIL=404;
+    public static final int SUCCESS=200;
+
 }
